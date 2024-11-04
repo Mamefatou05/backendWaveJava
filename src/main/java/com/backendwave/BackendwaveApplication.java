4c398fc4fd301319d377b947d6e78e114e390fad
fatou <fatouniangbasse@gmail.com>
2024-11-04 20:06:31 +0000

    initialiser

---
+++ src/main/java/com/backendwave/BackendwaveApplication.java
@@ -0,0 +1,13 @@
+package com.backendwave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendwaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendwaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backendwave/BackendwaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendwave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendwaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
